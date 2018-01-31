da933d57c278117afee49a229d15d8ef11f2e1b0
Dariusz Rosiek <daro.krk@interia.pl>
2018-01-31 12:13:01 +0100

    Init project files, and added script in program.js

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+
+// ustawienie nasluchiwania na zdarzenia odczytu
+
+process.stdin.on('readable', function() {
+	// metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch (instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/info':
+				process.stdout.write('Your version Node.js: ' + process.versions.node + '\n' + 'System Language : ' + process.env.LANG);
+				process.exit();
+				break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		}
+	}
+});
