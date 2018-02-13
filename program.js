2001e309b5099fdc911ff168e3af1cb989455238
Dariusz Rosiek <daro.krk@interia.pl>
2018-02-13 15:52:40 +0100

    Modified program.js, deleted necessary code line

--- program.js
+++ program.js
@@ -1,3 +1,7 @@
+// importujemy modul
+
+var os = require('os');
+
 process.stdin.setEncoding('utf-8');
 
 // ustawienie nasluchiwania na zdarzenia odczytu
@@ -9,12 +13,11 @@ process.stdin.on('readable', function() {
 		var instruction = input.toString().trim();
 		switch (instruction) {
 			case '/exit':
-				process.stdout.write('Quitting app!\n');
+				process.stdout.write('Quitting app!');
 				process.exit();
 				break;
 			case '/info':
-				process.stdout.write('Your version Node.js: ' + process.versions.node + '\n' + 'System Language : ' + process.env.LANG);
-				process.exit();
+				process.stdout.write('Your version Node.js: ' + process.versions.node + '\n' + 'System Language : ' + process.env.LANG + '\n');
 				break;
 			default:
 				process.stderr.write('Wrong instruction!\n');
